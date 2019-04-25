7cced0173146da0c18789eba64224bac204c44f0
Lucian Daivdescu <lucian.davidecu@yahoo.ro>
2019-04-25 20:31:29 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import * as serviceWorker from './serviceWorker';
 import HeaderLayout from "./components/layout/HeaderLayout";
 import HomeBanner from "./components/layout/HomeBanner";
 import HotDeals from "./components/layout/HotDeals";
+import ClientsLogo from "./components/layout/ClientsLogo";
 
 ReactDOM.render(<HeaderLayout />,
     document.getElementById('headerLayout'));
@@ -13,6 +14,11 @@ ReactDOM.render(<HomeBanner />,
 
 ReactDOM.render(<HotDeals />,
     document.getElementById('hotDeals'));
+
+ReactDOM.render(<ClientsLogo />,
+    document.getElementById('clientsLogo'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
