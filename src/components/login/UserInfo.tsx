68e752b50e0439b4c544c97984f21f80f6d21a83
Lucian D <lucian.davidescu@yahoo.ro>
2019-04-30 10:34:41 +0300

    added user info

---
+++ src/components/login/UserInfo.tsx
@@ -0,0 +1,32 @@
+
+
+
+<div className="section-top-border">
+    <div className="row">
+        <div className="col-lg-8 col-md-8">
+            <h3 className="mb-30 title_color">Form Element</h3>
+            <form action="#">
+                <div className="mt-10">
+                    <input type="text" name="first_name" placeholder="First Name" onFocus="this.placeholder = ''"
+                           onBlur="this.placeholder = 'First Name'"
+                           required className="single-input"/>
+                </div>
+                <div className="mt-10">
+                    <input type="text" name="last_name" placeholder="Last Name" onFocus="this.placeholder = ''"
+                           onBlur="this.placeholder = 'Last Name'"
+                           required className="single-input"/>
+                </div>
+                <div className="mt-10">
+                    <input type="text" name="last_name" placeholder="Last Name" onFocus="this.placeholder = ''"
+                           onBlur="this.placeholder = 'Last Name'"
+                           required className="single-input"/>
+                </div>
+                <div className="mt-10">
+                    <input type="email" name="EMAIL" placeholder="Email address" onFocus="this.placeholder = ''"
+                           onBlur="this.placeholder = 'Email address'"
+                           required className="single-input"/>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
