c2389931f90d1aae1b18d51dc9bdadc63a269a4c
Lucian Daivdescu <lucian.davidecu@yahoo.ro>
2019-08-11 23:13:14 +0300

    updated product detail

--- src/components/products/Shop.tsx
+++ src/components/products/Shop.tsx
@@ -111,54 +111,15 @@ class Shop extends React.Component<IProductProps, IProductInfoState> {
                 </Modal>
                 <Modal visible={this.state.visible} effect="fadeInUp"
                        onClickAway={() => this.closeModal()}>
-                    <div className={"container-fluid "}>
-                        <article className="row blog_item">
-                            <div className="col-md-3">
-                                <div className="blog_info text-right">
-                                    <div className="post_tag">
-                                        <a className="active" href={emptyHrefLink}>{this.props.category}</a>
-                                    </div>
-                                    <ul className="blog_meta list">
-                                        <li>
-                                            <a href={emptyHrefLink}>Mark wiens
-                                                <i className="lnr lnr-user"/>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href={emptyHrefLink}>12 Dec, 2017
-                                                <i className="lnr lnr-calendar-full"/>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href={emptyHrefLink}>1.2M Views
-                                                <i className="lnr lnr-eye"/>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href={emptyHrefLink}>06 Comments
-                                                <i className="lnr lnr-bubble"/>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="col-md-9">
-                                <div className="blog_post p_30">
-                                    <img src={this.props.logoSrc} alt=""/>
-                                    <div className="blog_details">
-                                        <a href={emptyHrefLink}>
-                                            <h2>{this.props.name}</h2>
-                                        </a>
-                                        <p>MCSE boot camps have its supporters and its detractors. Some people do not
-                                            understand
-                                            why you should have to spend money on boot camp when you can get the MCSE
-                                            study
-                                            materials yourself at a fraction.</p>
-                                        <a href={this.props.mainUrl} className="white_bg_btn">Access</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </article>
+                    <div className="text-center p_20">
+                        <img src={this.props.logoSrc} alt=""/>
+                        <div className="blog_details">
+                            <a href={emptyHrefLink}>
+                                <h2>{this.props.name}</h2>
+                            </a>
+                            <h2>{"Category: " + this.props.category}</h2>
+                            <a href={this.props.mainUrl} className="white_bg_btn">Access</a>
+                        </div>
                     </div>
                 </Modal>
                 <div className="col-lg-3 col-md-3 col-sm-6">
