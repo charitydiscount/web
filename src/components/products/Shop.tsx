081e773826bb6a3097610225bfc91c8572cb8bc5
Lucian Daivdescu <lucian.davidecu@yahoo.ro>
2019-08-25 23:10:41 +0300

    moved favorite shop mechanism

--- src/components/products/Shop.tsx
+++ src/components/products/Shop.tsx
@@ -1,143 +1,139 @@
 import * as React from 'react';
 import Modal from 'react-awesome-modal';
-import { emptyHrefLink } from '../../helper/Constants';
+import {emptyHrefLink} from '../../helper/Constants';
 import {
-  isInFavoriteShops,
-  updateFavoriteShops,
+    isInFavoriteShops,
+    updateFavoriteShops,
 } from '../../rest/ShopsService';
-import { computeUrl } from '../../helper/AppHelper';
+import {computeUrl} from '../../helper/AppHelper';
 
 interface IProductInfoState {
-  visible: boolean;
-  fShopVisible: boolean;
-  favShop: boolean;
+    visible: boolean;
+    fShopVisible: boolean;
+    favShop: boolean;
 }
 
 interface IProductProps {
-  logoSrc: string;
-  name: string;
-  id: number;
-  category: string;
-  mainUrl: string;
-  uniqueCode: string;
+    logoSrc: string;
+    name: string;
+    id: number;
+    category: string;
+    mainUrl: string;
+    uniqueCode: string;
 }
 
 class Shop extends React.Component<IProductProps, IProductInfoState> {
-  constructor(props: IProductProps) {
-    super(props);
-    this.state = {
-      visible: false,
-      fShopVisible: false,
-      favShop: false,
-    };
-    this.updateFavoriteShops = this.updateFavoriteShops.bind(this);
-    isInFavoriteShops(this.props.id, this);
-  }
+    constructor(props: IProductProps) {
+        super(props);
+        this.state = {
+            visible: false,
+            fShopVisible: false,
+            favShop: false,
+        };
+        this.updateFavoriteShops = this.updateFavoriteShops.bind(this);
+        isInFavoriteShops(this.props.id, this);
+    }
 
-  closeModal() {
-    this.setState({
-      visible: false,
-    });
-  }
+    closeModal() {
+        this.setState({
+            visible: false,
+        });
+    }
 
-  closeFShopModal() {
-    this.setState({
-      fShopVisible: false,
-      favShop: true,
-    });
-  }
+    closeFShopModal() {
+        this.setState({
+            fShopVisible: false,
+            favShop: true,
+        });
+    }
 
-  openFShopModal() {
-    this.setState({
-      fShopVisible: true,
-    });
-  }
+    openFShopModal() {
+        this.setState({
+            fShopVisible: true,
+        });
+    }
 
-  openModal() {
-    this.setState({
-      visible: true,
-    });
-  }
+    openModal() {
+        this.setState({
+            visible: true,
+        });
+    }
 
-  /**
-   * Used to add favorite shops to DB
-   */
-  public updateFavoriteShops() {
-    this.openFShopModal();
-    updateFavoriteShops(this.props.name);
-  }
+    /**
+     * Used to add favorite shops to DB
+     */
+    public updateFavoriteShops() {
+        this.closeModal()
+        this.openFShopModal();
+        updateFavoriteShops(this.props.name);
+    }
 
-  public render() {
-    return (
-      <React.Fragment>
-        <Modal
-          visible={this.state.fShopVisible}
-          effect="fadeInUp"
-          onClickAway={() => this.closeFShopModal()}
-        >
-          <h3 style={{ padding: 15 }}>
-            Favorite shop: {this.props.name} added
-          </h3>
-        </Modal>
-        <Modal
-          visible={this.state.visible}
-          effect="fadeInUp"
-          onClickAway={() => this.closeModal()}
-        >
-          <div className="text-center p_20">
-            <img src={this.props.logoSrc} alt="" />
-            <div className="blog_details">
-              <a href={emptyHrefLink}>
-                <h2>{this.props.name}</h2>
-              </a>
-              <h3>{'Category: ' + this.props.category}</h3>
-              <a
-                href={computeUrl(this.props.uniqueCode, this.props.mainUrl)}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="white_bg_btn"
-              >
-                Access
-              </a>
-            </div>
-          </div>
-        </Modal>
-        <div className="col-lg-3 col-md-3 col-sm-6">
-          <div className="f_p_item">
-            <div className="f_p_img">
-              <a href={emptyHrefLink} onClick={() => this.openModal()}>
-                <img
-                  className="img-fluid img-min img"
-                  src={this.props.logoSrc}
-                  alt=""
-                />
-              </a>
-
-              <div
-                className={
-                  this.state.favShop === true ? 'p_iconUpdate' : 'p_icon'
-                }
-              >
-                <a
-                  href={emptyHrefLink}
-                  onClick={
-                    this.state.favShop === true
-                      ? undefined
-                      : this.updateFavoriteShops
-                  }
+    public render() {
+        return (
+            <React.Fragment>
+                <Modal
+                    visible={this.state.fShopVisible}
+                    effect="fadeInUp"
+                    onClickAway={() => this.closeFShopModal()}
                 >
-                  <i className="lnr lnr-heart" />
-                </a>
-              </div>
-            </div>
-            <a href={emptyHrefLink} onClick={() => this.openModal()}>
-              <h4>{this.props.name}</h4>
-            </a>
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+                    <h3 style={{padding: 15}}>
+                        Favorite shop: {this.props.name} added
+                    </h3>
+                </Modal>
+                <Modal
+                    visible={this.state.visible}
+                    effect="fadeInUp"
+                    onClickAway={() => this.closeModal()}
+                >
+                    <div className="text-center p_20">
+                        <img src={this.props.logoSrc} alt=""/>
+                        <div className="blog_details">
+                            <a href={emptyHrefLink}>
+                                <h2>{this.props.name}</h2>
+                            </a>
+                            <h3>{'Category: ' + this.props.category}</h3>
+                            <div className="s_product_text">
+                                <div className="card_area">
+                                    <a href={computeUrl(this.props.uniqueCode, this.props.mainUrl)}
+                                       target="_blank"
+                                       rel="noopener noreferrer"
+                                       className="main_btn">
+                                        Access
+                                    </a>
+                                    <div className={this.state.favShop === true ? 'icon_btn p_iconUpdate' : 'icon_btn p_icon'}>
+                                        <a href={emptyHrefLink} onClick={
+                                                this.state.favShop === true
+                                                    ? undefined
+                                                    : this.updateFavoriteShops
+                                            }
+                                        >
+                                            <i className="lnr lnr-heart"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
+                <div className="col-lg-3 col-md-3 col-sm-6">
+                    <div className="f_p_item">
+                        <div className="f_p_img">
+                            <a href={emptyHrefLink} onClick={() => this.openModal()}>
+                                <img
+                                    className="img-fluid img-min img"
+                                    src={this.props.logoSrc}
+                                    alt=""
+                                />
+                            </a>
+                        </div>
+                        <a href={emptyHrefLink} onClick={() => this.openModal()}>
+                            <h4>{this.props.name}</h4>
+                        </a>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Shop;
