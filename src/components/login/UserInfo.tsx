d02dcacf786cfe21b9db56165f8de5036063f77c
Lucian D <lucian.davidescu@yahoo.ro>
2019-08-25 14:43:32 +0300

    changed user info visual aspect

--- src/components/login/UserInfo.tsx
+++ src/components/login/UserInfo.tsx
@@ -1,52 +1,53 @@
-import GenericInput from '../input/GenericInput';
-import { InputType } from '../../helper/Constants';
-import { store } from '../../index';
-import { NavigationsAction } from '../../redux/actions/NavigationsAction';
-import { Stages } from '../helper/Stages';
+import {store} from '../../index';
+import {NavigationsAction} from '../../redux/actions/NavigationsAction';
+import {Stages} from '../helper/Stages';
 import * as React from 'react';
 
 class UserInfo extends React.Component {
-  public componentDidMount() {
-    store.dispatch(NavigationsAction.setStageAction(Stages.USER));
-  }
+    public componentDidMount() {
+        store.dispatch(NavigationsAction.setStageAction(Stages.USER));
+    }
 
-  public componentWillUnmount() {
-    store.dispatch(NavigationsAction.resetStageAction(Stages.USER));
-  }
+    public componentWillUnmount() {
+        store.dispatch(NavigationsAction.resetStageAction(Stages.USER));
+    }
 
-  public render() {
-    return (
-      <div className="container p_120">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <h3 className="mb-30 title_color">User info</h3>
-            <div className="mt-10">
-              <GenericInput
-                type={InputType.TEXT}
-                id={'name'}
-                placeholder={'Name'}
-                className={'single-input'}
-              />
+    public render() {
+        return (
+
+            <div className="product_image_area">
+                <div className="container p_120">
+                    <div className="row s_product_inner">
+                        <div className="col-lg-4"/>
+                        <div className="col-lg-4">
+                            <div className="s_product_img">
+                                <div className="blog_right_sidebar">
+                                    <aside className="single_sidebar_widget author_widget">
+                                        <img className="author_img rounded-circle" src="img/blog/author.png"
+                                             alt=""/>
+                                        <h4>Charlie Barber</h4>
+                                        <p>Senior blog writer</p>
+                                        <div className="br"></div>
+                                    </aside>
+                                    <aside className="single_sidebar_widget popular_post_widget">
+                                        <div className="col-md-12 text-center p_05">
+                                            <a href={"#"} className="btn submit_btn">Change password</a>
+                                        </div>
+                                        <div className="col-md-12 text-center p_05">
+                                            <a href={"#"} className="btn submit_btn">Change password</a>
+                                        </div>
+                                        <div className="col-md-12 text-center p_05">
+                                            <a href={"#"} className="btn submit_btn">Change password</a>
+                                        </div>
+                                    </aside>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="mt-10">
-              <GenericInput
-                type={InputType.TEXT}
-                id={'email'}
-                placeholder={'Email'}
-                className={'single-input'}
-              />
-            </div>
-          </div>
-        </div>
-        <br />
-        <div className="col-md-12 text-center">
-          <button type="submit" value="submit" className="btn submit_btn">
-            Update
-          </button>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default UserInfo;
