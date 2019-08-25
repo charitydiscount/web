f76c4f0fe236a724e20711ecf2f984a4e108b9fe
Lucian D <lucian.davidescu@yahoo.ro>
2019-08-25 14:05:43 +0300

    improve visual aspect

--- src/components/causes/Cause.tsx
+++ src/components/causes/Cause.tsx
@@ -12,18 +12,19 @@ class Cause extends React.Component<ICauseProps> {
 
     public render() {
         var imagesList = this.props.images ? this.props.images.map(image => {
-            return <img src={image.url} alt="" className="img-fluid"/>
+            return <img src={image.url} alt="" width={300} height={300} className="img-fluid"/>
         }) : null;
 
         return (
-            <div>
+            <div className={"p_20"}>
                 <h3 className="mb-30 title_color">{this.props.title}</h3>
                 <div className="row">
-                    <div className="col-md-3">
-                        {imagesList}
-                    </div>
-                    <div className="col-md-9 mt-sm-20 left-align-p">
+                    <div className="mt-sm-20 left-align-p">
                         <p>{this.props.description}</p>
+                    </div>
+
+                    <div>
+                        {imagesList}
                     </div>
                 </div>
             </div>
