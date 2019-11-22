72f7adfe5e8da85c1fc1a143b2dc7fec5fd948f8
Lucian Daivdescu <lucian.davidecu@yahoo.ro>
2019-11-23 00:27:10 +0200

    #159 - added adblock button

--- src/ReactAdBlock.tsx
+++ src/ReactAdBlock.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AdBlockDetect from 'react-ad-block-detect';
 import Modal from 'react-awesome-modal';
 import {FormattedMessage} from 'react-intl';
+import {emptyHrefLink} from "./helper/Constants";
 
 class ReactAdBlock extends React.Component {
 
@@ -11,6 +12,10 @@ class ReactAdBlock extends React.Component {
                 <Modal visible={true} effect="fadeInUp">
                     <h3 style={{padding: 15}}>
                         <FormattedMessage id={"adblock.message"} defaultMessage="Disable AdBlock or similar programs to continue"/>
+                        <a href={emptyHrefLink} onClick={() => window.location.reload()}
+                            className="btn submit_btn genric-btn circle">
+                            <FormattedMessage id={"adblock.button"} defaultMessage="Reload"/>
+                        </a>
                     </h3>
                 </Modal>
             </AdBlockDetect>
