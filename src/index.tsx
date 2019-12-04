1f4380273c35d8feb3977754f6409f566198b179
Lucian Daivdescu <lucian.davidecu@yahoo.ro>
2019-12-04 20:57:17 +0200

    changed app version

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ import {LoginDto} from "./components/login/LoginComponent";
 import I18nApp from "./I18nApp";
 
 export const publicUrl = process.env.PUBLIC_URL || '';
-export const appVersion = "1.3.0";
+export const appVersion = "1.3.1";
 
 // REDUX----------------------------------------------------------------------------------------------------------------
 const initialState = {};
