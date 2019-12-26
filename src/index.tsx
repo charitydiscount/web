faee0c05a8058df561d72078090f42d7cb931f53
Lucian D <lucian.davidescu@yahoo.ro>
2019-12-26 14:44:07 +0200

    added css back

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,7 @@ import { LoginDto } from './components/login/LoginComponent';
 import I18nApp from './I18nApp';
 
 export const publicUrl = process.env.PUBLIC_URL || '';
-export const appVersion = '1.7.0';
+export const appVersion = '1.7.1';
 
 // REDUX----------------------------------------------------------------------------------------------------------------
 const initialState = {};
