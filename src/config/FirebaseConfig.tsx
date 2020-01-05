c7a8ec65dd56475918eb4564a26cc6cd6c3ae1c3
Andrei Stefanie <andrei.stefanie@gmail.com>
2020-01-05 15:53:39 +0200

    Get firebase config from env

--- src/config/FirebaseConfig.tsx
+++ src/config/FirebaseConfig.tsx
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: 'AIzaSyBM06U5MJiIlIAySiyh8_eAGUMAWYYhyUA',
-    authDomain: 'charitydiscount.firebaseapp.com',
-    databaseURL: 'https://charitydiscount.firebaseio.com',
-    projectId: 'charitydiscount',
-    storageBucket: 'charitydiscount.appspot.com',
-    messagingSenderId: '548055517349',
-    appId: '1:548055517349:web:417aa24fdaf6fbc53e654c',
-    measurementId: 'G-XJMF0X7VCV',
+    apiKey: process.env.REACT_APP_FB_API_KEY,
+    authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FB_DB_URL,
+    projectId: process.env.REACT_APP_FB_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FB_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_FB_APP_ID,
+    measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
