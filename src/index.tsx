efaff6b230df302c81286c5362591cd368bab536
Lucian D <lucian.davidescu@yahoo.ro>
2020-05-03 12:44:46 +0300

    updated version

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ import { AuthActions } from './components/login/UserActions';
 import I18nApp from './I18nApp';
 
 export const publicUrl = process.env.PUBLIC_URL || '';
-export const appVersion = '2.2.6';
+export const appVersion = '2.2.7';
 
 // REDUX----------------------------------------------------------------------------------------------------------------
 const initialState = {};
